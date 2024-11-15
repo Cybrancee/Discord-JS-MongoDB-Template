7a176fa38d3353c0ffcbf32bf13c257de8b3fb7f
vaq <maxmari395@gmail.com>
2024-11-15 21:24:26 +0100

    feat: 🔥 removed functions that require db

--- src/Schemas.js/DevPermission.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-let devPermission = new Schema({
-    DevId: String,
-    Permission: String,
-});
-
-module.exports = model("Developer", devPermission);
